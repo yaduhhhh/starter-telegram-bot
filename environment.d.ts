ff37d95ba071601f8e89b25e9bdea8395e909c1c
yaduhhhh <165858222+yaduhhhh@users.noreply.github.com>
2024-04-03 07:17:07 +0530

    Update environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      TELEGRAM_TOKEN: string;
+      TELEGRAM_TOKEN: 6883046798:AAHSVMDA8XqRERzM6FGADmriNtDiWa00Lws;
       NODE_ENV?: "development" | "production";
       PORT?: number;
     }
